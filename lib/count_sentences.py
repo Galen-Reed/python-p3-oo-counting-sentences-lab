5f0e24cfc6c13eee78641cdf0cccc1db066320e8
GalenReed <galenreed@vt.edu>
2024-12-16 20:57:09 -0500

    class

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        if type(value) == str:
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return "." in self._value
+
+    def is_question(self):
+        return "?" in self._value
+    
+    def is_exclamation(self):
+        return "!" in self._value
+    
+    def count_sentences(self):
+        sentences = [s.strip() for s in self._value.split() if s.strip()]
+        sentence_count = 0
+        for sentence in sentences:
+            if sentence.endswith(('.', '!', '?')):
+                sentence_count += 1
+        return sentence_count
